2df329517de9cfabbb108e83d1999767ee8e75c0
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-02-24 15:31:41 +0100

    setting up a Redux store, with messages in initial state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import Chatroom from './components/Chatroom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import store from "./store/index";
+
+import "./styles/index.scss";
+import Chatroom from "./components/Chatroom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Chatroom />
+    <Provider store={store}>
+      <Chatroom />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/reducers/chatReducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  messages: [
+    {
+      id: 1,
+      username: 'Super Chat',
+      content: 'Salut ça va ?',
+    },
+    {
+      id: 2,
+      username: 'Super Chat',
+      content: 'T\'as pas des super croquettes ?',
+    },
+    {
+      id: 3,
+      username: 'Super Chat',
+      content: 'Stp',
+    },
+  ],
+};
+
+function nameForTheReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default nameForTheReducer;
