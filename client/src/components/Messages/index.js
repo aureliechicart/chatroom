51fd4f8c1bf06f00bd7fb26a2944c4f4489e24d7
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-02-24 23:10:38 +0100

    setting up automatic scroll to the end of messages zone upon each new message

--- client/src/components/Messages/index.js
+++ client/src/components/Messages/index.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
-import './messages.scss';
-import Message from '../../containers/Messages/Message';
+import "./messages.scss";
+import Message from "../../containers/Messages/Message";
 
-const Messages = ({ messages }) => (
-  <div className="messages">
-    {messages.map((message) => (
-      <Message key={message.id} {...message}/>
-    ))}
-  </div>
-);
+const Messages = ({ messages }) => {
+  const refContainer = useRef(null);
+
+  useEffect(() => {
+    refContainer.current.scrollTo({
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight
+      top: refContainer.current.scrollHeight,
+      left: 0,
+      // progressive scroll
+      behavior: "smooth",
+    });
+  });
+  return (
+    <div className="messages" ref={refContainer}>
+      {messages.map((message) => (
+        <Message key={message.id} {...message} />
+      ))}
+    </div>
+  );
+};
 
 Messages.propTypes = {
   messages: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-    }).isRequired,
+    }).isRequired
   ).isRequired,
 };
 
