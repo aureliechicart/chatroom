8acea543425c6a9df61ed7f0c1d0d5567d9f11ce
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-02-24 22:20:15 +0100

    subscribing to send_message event on the websocket

--- client/src/middleware/authMiddleware.js
+++ client/src/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ import { SUBMIT_LOGIN, connectUser, displayError } from '../actions/chat';
 const authMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case SUBMIT_LOGIN:
-      axios.post('http://localhost:3001/login', {
+      axios.post('/login', {
         email: store.getState().email,
         password: store.getState().password,
       })
@@ -15,6 +15,7 @@ const authMiddleware = (store) => (next) => (action) => {
           store.dispatch(saveAction);
         })
         .catch((error) => {
+          console.log(error);
           store.dispatch(displayError(error.message));
         });
 
--- client/src/middleware/socketMiddleware.js
+++ client/src/middleware/socketMiddleware.js
@@ -1,13 +1,21 @@
-import { CONNECT_WEBSOCKET } from '../actions/chat';
+import { CONNECT_WEBSOCKET, CONNECT_USER } from "../actions/chat";
+
+let socket;
 
 const socketMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
-    case CONNECT_WEBSOCKET: {
+    case CONNECT_WEBSOCKET:
       // proxy defined in package.json to redirect any unknow requests to our API server
-      const socket = window.io('/');
-      console.log('websocket créé !');
+      socket = window.io("/");
+      console.log(socket);
+      console.log("websocket créé !");
       break;
-    }
+
+    case CONNECT_USER:
+      socket.on("send_message", (message) => {
+        console.log("nouveau message :", message);
+      });
+      break;
 
     default:
   }
