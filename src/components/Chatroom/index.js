49c44f961b87d76be6584621ee9ede25d09cf672
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-02-24 14:42:44 +0100

    setting up root component

---
+++ src/components/Chatroom/index.js
@@ -0,0 +1,13 @@
+import logo from '../../logo.svg';
+import './styles.css';
+
+const Chatroom = () => {
+  return (
+    <div className="app">
+    <img src={logo} alt="react logo" />
+    <h1>Composant : App</h1>
+  </div>
+  );
+}
+
+export default Chatroom;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Chatroom from './components/Chatroom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Chatroom />
   </React.StrictMode>,
   document.getElementById('root')
 );
